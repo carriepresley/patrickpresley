04817e94577236e502d6df2cf81c0e586b5575b2
carriepresley <carriepresley15@gmail.com>
2020-12-21 17:34:32 -0600

    deploy polish

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,7 +27,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
+            <span className="text-color-main">{name || 'Patrick Presley'}</span>
             <br />
             {subtitle || "I'm a Ray-Ban rep."}
           </h1>
